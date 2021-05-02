7af84674a860c9151d3ca89a80e255a3364c9d26
minna <minnaminta@gmail.com>
2021-05-02 08:08:00 +0000

    add about us, contact us, index.html, manifest hostname

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    path(route='about/', view=views.about, name='about-view'),
+    path(route='contact/', view=views.contact, name='contact-view'),
 
     # path for dealer reviews view
 
